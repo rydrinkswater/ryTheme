0ce25ce26444965e2d2c790b83ce6dca05c563fd
GitHub Action <action@github.com>
2022-05-26 04:36:35 +0000

    Template cleanup

--- src/main/kotlin/com/github/rydrinkswater/rytheme/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/rydrinkswater/rytheme/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.rydrinkswater.rytheme.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.rydrinkswater.rytheme.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/rydrinkswater/rytheme/services/MyProjectService.kt
+++ src/main/kotlin/com/github/rydrinkswater/rytheme/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.rydrinkswater.rytheme.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.rydrinkswater.rytheme.MyBundle
 
 class MyProjectService(project: Project) {
 
